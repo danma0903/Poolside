6a83d2b626c7782cc94382f4f0818b678fbd8711
Anthony Leong <aj.leong623@gmail.com>
2025-10-18 23:40:37 -0700

    Add YOLO object detection to video streaming server
    Integrate YOLO model for object detection in video stream and send alerts based on detected targets.

--- server/video_streaming_backend/video_stream_server.py
+++ server/video_streaming_backend/video_stream_server.py
@@ -2,6 +2,10 @@ import asyncio
 import websockets
 import cv2
 import base64
+from ultralytics import YOLO
+import requests
+
+model = YOLO("best1.pt")
 
 async def video_stream(websocket):
     cap = cv2.VideoCapture("data/trimmed_video.mp4")
@@ -10,9 +14,34 @@ async def video_stream(websocket):
         if not ret:
             break
 
+        # Apply YOLO model inference
+        results = model(frame)
+        annotated_frame = results[0].plot()
+
         # Encode frame as JPEG
-        _, buffer = cv2.imencode('.jpg', frame)
+        _, buffer = cv2.imencode('.jpg', annotated_frame)
         jpg_as_text = base64.b64encode(buffer).decode('utf-8')
+
+        target_detected = False
+        for box in results[0].boxes:
+            cls_index = int(box.cls[0])
+            class_name = model.names[cls_index]
+            # Replace person with intended target class.
+            if (class_name == "toddler"):
+                target_detected = True
+                break
+
+                
+        if target_detected:
+            requests.get("http://localhost:5000/test-alert")
+        else:
+            requests.get("http://localhost:5000/test-alert-relief")
+
+        
+        # Display frame
+        #cv2.imshow("YOLO Stream", annotated_frame)
+        if cv2.waitKey(1) & 0xFF == 13:  # Enter key to quit
+            break
 
         # Send frame as base64 string
         await websocket.send(jpg_as_text)
@@ -23,7 +52,7 @@ async def video_stream(websocket):
     cap.release()
 
 async def main():
-    async with websockets.serve(video_stream, "10.113.114.118", 8888):
+    async with websockets.serve(video_stream, "127.0.0.1", 8888):
         print("WebSocket server started on ws://10.113.114.118:8888")
         await asyncio.Future()  # Run forever
 
